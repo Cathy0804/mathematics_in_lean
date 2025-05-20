d239f2cd34519cfe16963222829db5cc5397c536
Cathy0804 <cathylibingyan@outlook.com>
2025-05-20 20:39:11 +0100

    2.3 completed

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -43,8 +43,13 @@ example (x : ℝ) : x ≤ x :=
 #check (lt_trans : a < b → b < c → a < c)
 
 -- Try this.
+example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e :=
+  lt_trans (lt_of_lt_of_le (lt_of_le_of_lt h₀ h₁) h₂) h₃
+
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀ -- goal: a < e, given: a ≤ b, next goal: b < e
+  apply lt_trans h₁ -- goal: b < e, given: b < c, next goal: c < e
+  exact lt_of_le_of_lt h₂ h₃ -- goal c < e, given: c ≤ d, d < e, all goals accomplished!
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -86,21 +91,30 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  apply add_le_add_left
+  exact h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by linarith [exp_pos a]
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  exact h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  refine tsub_le_tsub_left ?_ c
+  apply exp_le_exp.mpr
+  exact h
+
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -121,7 +135,17 @@ example : 2*a*b ≤ a^2 + b^2 := by
   linarith
 
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  apply abs_le'.mpr
+  constructor
+  · have h : 0 ≤ a^2 - 2*a*b + b^2
+    calc
+      a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
+      _ ≥ 0 := by apply pow_two_nonneg
+    linarith
+  · have h' : 0 ≤ a^2 + 2*a*b + b^2
+    calc
+      a^2 + 2*a*b + b^2 = (a + b)^2 := by ring
+      _ ≥ 0 := by apply pow_two_nonneg
+    linarith
 
 #check abs_le'.mpr
-
